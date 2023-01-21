8ec8660af6f927f280b87f6bc1ebf26257d5a4bc
MacroHEX <steam18py@hotmail.com>
2023-01-21 11:03:24 -0300

    Ocultado Banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,7 +1,7 @@
 export function Banner() {
   return (
     <div className="felx flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-10">
-      <div>
+      {/* <div>
         <h1 className="text-7xl py-5">Ogamikami</h1>
         <h2 className="mt-5 md:mt-0 py-5">
           Welcome to{" "}
@@ -15,7 +15,7 @@ export function Banner() {
       <p className="mt-5 md:mt-2 text-gray-400 max-w-sm">
         Nef product feautres | The lastes int technology | The weekly debugging
         nightmares & More!
-      </p>
+      </p> */}
     </div>
   );
 }
